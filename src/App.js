4c9ebfbb5cd21996ae497e2fdb028918002b3a13
Sarah <Sarah.almarri.76@gmail.com>
2023-12-26 19:57:07 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import Nav from './components/Nav';
-import Home from './components/Home';
-import TripsList from './components/TripsList';
-import TripDetail from './components/TripDetail';
+import "./App.css";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import TripsList from "./components/TripsList";
+import TripDetail from "./components/TripDetail";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div id="page-top">
       <Nav />
-      <Home />
+      {/* <Home />
       <TripsList />
-      <TripDetail />
+      <TripDetail /> */}
+      <Outlet />
     </div>
   );
 }
